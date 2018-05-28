92603162ce82a006bac7983f56e0f219614839b5
castillobueso <castillobueso@yahoo.com>
2018-05-28 11:12:27 -0300

    Criando e exibindo uma lista no aplicativo usando AngularJS

---
+++ www/js/app.js
@@ -0,0 +1,32 @@
+var app = angular.module('todoapp', ['ionic']);
+
+app.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+});
+
+app.controller('ListaCtrl', function($scope) {
+
+  $scope.tarefas = [
+    {
+      "texto" : "Realizar as atividades do curso",
+      "data"  : new Date(),
+      "feita" : false
+    },
+    {
+      "texto" : "Passera com o cachorro",
+      "data"  : new Date(),
+      "feita" : true
+    }
+
+  ];
+
+});
